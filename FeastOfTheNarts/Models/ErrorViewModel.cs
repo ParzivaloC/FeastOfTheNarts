0920b923771eb5ddd79ec68a4269c5eb70099c95
ParzivaloC <bicherahovartur12@mail.ru>
2026-05-11 22:15:17 +0300

    Добавьте файлы проекта.

---
+++ FeastOfTheNarts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeastOfTheNarts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
