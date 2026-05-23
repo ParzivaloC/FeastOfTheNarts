a1831a07ee231ed1cd5390505a6d3601004f86d1
ParzivaloC <bicherahovartur12@mail.ru>
2026-05-23 22:06:12 +0300

    1 Базовый каркас Создано новое решение FeastOfTheNarts.sln.(так как прошлое решение было сделано не правильно , пришлось удалить)
    Инициализирована трехслойная структура проектов в папке src/:
    
    FeastOfTheNarts.Core — правила игры на C# и вся логика проекта.
    
    FeastOfTheNarts.Infrastructure — слой для будущей работы с БД и внешними сервисами.
    
    FeastOfTheNarts.Web — ASP.NET Core Web API сервер.
    
    далее проект Web успешно подключен к ядру Core через ссылки проектов для правильной работы .
    
    2. Сетевой слой и Конфигурация бэкенда
    подключена маршрутизация контроллеров в Program.cs
    
    Добавлена и настроена поддержка SignalR для обеспечения real-time взаимодействия игроков.
    
    Создан базовый GameHub с первым методом JoinMatch для будущей очереди поиска игроков.
    
    3. Доменная модель игры (Ядро)
    Внедрены базовые перечисления игровых сущностей: Faction (роды нартов: Ахсартаггата, Бората, Алагата, Нейтральные) и CardRow (боевые ряды: Melee, Ranged, Siege, Any).
    
    Разработана объектная модель карт с использованием модификатора init для обеспечения иммутабельности (неизменяемости данных карт после инициализации):
    
    BaseCard — абстрактный корень для всех типов карт.
    
    UnitCard — класс боевого юнита с поддержкой силы, привязки к ряду и флага героя (IsHero).
    
    Создана модель PlayerState для отслеживания состояния участника матча (контроль жизней, статуса паса, а также логика зон: Колода, Рука, Сброс и метод взятия карты DrawCard).

---
+++ src/FeastOfTheNarts.Core/Domain/Models/UnitCard.cs
@@ -0,0 +1,13 @@
+﻿using FeastOfTheNarts.Core.Domain.Enums;
+using FeastOfTheNarts.Core.Domain.Enums.Models;
+
+namespace FeastOfTheNarts.Core.Domain.Models
+{
+    public class UnitCard : BaseCard
+    {
+        public int BasePower { get; init; }
+        public CardRow TargetRow { get; init; }
+        public bool IsHero { get; init; }
+
+    }
+}
