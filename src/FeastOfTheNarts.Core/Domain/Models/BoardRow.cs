e4f3ded2ebf97b3afbc9a181dd2dd63f33683ce0
ParzivaloC <bicherahovartur12@mail.ru>
2026-05-24 20:24:48 +0300

    Добавил обработку стола . 1. Класс BoardRow - Самая мелкая еденица стола , берем конкретный ряд который хранит список лежащих в нем карт и считает собственную силу.
    2. Класс PlayerBoard - у каждого игрока есть своя половина поля которая состоит ровно из трех рядов (ближнего , дальнего , осадного) . Этот класс управляет выкладыванием карт на сторону конкретного игрока .
    
    3. Класс GameBoard - Общее поле . Когда два игрока вступают в игру , сервер создает одно большое поле для обоих игроков , внутри него лежат две половины поля каждого из игроков

---
+++ src/FeastOfTheNarts.Core/Domain/Models/BoardRow.cs
@@ -0,0 +1,22 @@
+﻿using FeastOfTheNarts.Core.Domain.Enums;
+
+namespace FeastOfTheNarts.Core.Domain.Models
+{
+    public class BoardRow
+    {
+        public CardRow Type { get; }
+
+        public List<UnitCard> Cards { get; set; } = new List<UnitCard>();// Список карт, размещенных в этом ряду
+
+        public BoardRow(CardRow type)
+        {
+            Type = type;
+        }
+
+        // Метод для расчета общей силы карт в ряду
+        public int GetTotalPower()
+        {
+            return Cards.Sum(c => c.BasePower);
+        }
+    }
+}
